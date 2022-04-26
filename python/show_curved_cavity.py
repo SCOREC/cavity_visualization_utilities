ab0de6af0a55937d58d2f11e9c62ae57f4ae3d74
Morteza H. Siboni <hakimm2@rpi.edu>
2022-04-26 12:40:42 -0400

    Cleanup + fixes the error for vertex cavities

--- python/show_curved_cavity.py
+++ python/show_curved_cavity.py
@@ -7,123 +7,148 @@ parser.add_argument('-p', '--save_png', help="saves a png of the view", action="
 parser.add_argument('-m', '--show_mesh', help="show mesh", action="store_true")
 args = parser.parse_args()
 
+
+def get_vtu_path_for_mesh(root, vtu_phrase):
+  identifier='curved'
+  search_phrase=root+"/mesh/"+identifier+"/rep_tri_*"
+  all_ = glob.glob(search_phrase)
+  target_dir = ''
+  if (len(all_) == 1):
+    target_dir=all_[0]
+  else:
+    max_res=-1
+    max_idx=0
+    idx=0
+    for st in all_:
+      substs = st.split("_")
+      if substs[-2] > max_res:
+	max_res = substs[-2]
+	max_idx = idx
+      idx=idx+1
+    target_dir=all_[max_idx]
+  search_phrase=target_dir+"/"+vtu_phrase
+  all_vtus=glob.glob(search_phrase)
+
+  if (len(all_vtus) == 0):
+    print("no vtu was found in " + search_phrase + ". quiting!")
+    sys.exit("Exiting!")
+
+  target_vtu=''
+  if (len(all_vtus) == 1):
+    target_vtu=all_vtus[0]
+  else:
+    max_res=-1
+    max_idx=0
+    idx=0
+    for st in all_vtus:
+      substs = st.split("_")
+      if substs[-2] > max_res:
+	max_res = substs[-2]
+	max_idx = idx
+      idx=idx+1
+    target_vtu=all_vtus[max_idx]
+  return target_vtu
+# end of get_vtu_path_for_mesh
+
+def get_vtu_path_for_cavity(root, cavity_name, curved=True):
+  is_vertex_cavity=False
+  if cavity_name.split('_')[-2] == "vertex":
+    is_vertex_cavity=True
+  all_cavities = glob.glob(root+"/cavities/*")
+  all_cavities_short = [name.split("/")[-1] for name in all_cavities]
+  if cavity_name not in all_cavities_short:
+    print(cavity_name, "was not found in ", root+"/cavities")
+    print("available names are:")
+    print(all_cavities_short)
+    sys.exit("Exiting!")
+
+
+  if curved:
+    search_phrase=root+"/cavities/"+cavity_name+"/cavity_curved/rep_tri_*"
+    all_rep_tri_folders=glob.glob(search_phrase)
+
+    target_dir = ''
+    if (len(all_rep_tri_folders) == 1):
+      target_dir=all_rep_tri_folders[0]
+    else:
+      max_res=-1
+      max_idx=0
+      idx=0
+      for st in all_rep_tri_folders:
+	substs = st.split("_")
+	if substs[-2] > max_res:
+	  max_res = substs[-2]
+	  max_idx = idx
+	idx=idx+1
+      target_dir=all_rep_tri_folders[max_idx]
+
+    search_phrase=target_dir+"/vtu/order_*.vtu"
+    all_vtus=glob.glob(search_phrase)
+
+    if (len(all_vtus) == 0):
+      print("no vtu was found in ", search_phrase, ". quiting!")
+      sys.exit("Exiting!")
+
+
+    target_vtu=''
+    if (len(all_vtus) == 1):
+      target_vtu=all_vtus[0]
+    else:
+      max_res=-1
+      max_idx=0
+      idx=0
+      for st in all_vtus:
+	substs = st.split("_")
+	if substs[-2] > max_res:
+	  max_res = substs[-2]
+	  max_idx = idx
+	idx=idx+1
+      target_vtu=all_vtus[max_idx]
+
+    cavity_path=target_vtu
+
+    search_phrase=root+"/cavities/"+cavity_name+"/cavity_curved_wire/0/*.vtu"
+    vtu_files=glob.glob(search_phrase)
+    if (len(vtu_files) == 0):
+      print("no vtu was found in ", search_phrase, ". quiting!")
+      sys.exit("Exiting!")
+    cavity_wire_path=vtu_files[0]
+
+
+    if is_vertex_cavity:
+      search_phrase=root+"/cavities/"+cavity_name+"/entity_linear/0/*.vtu"
+    else:
+      search_phrase=root+"/cavities/"+cavity_name+"/entity_curved_wire/0/*.vtu"
+    entity_vtu=glob.glob(search_phrase)
+
+    if (len(entity_vtu) == 0):
+      print("no vtu was found in ", search_phrase, ". quiting!")
+      sys.exit("Exiting!")
+    entity_path=entity_vtu[0]
+  else:
+    search_phrase=root+"/cavities/"+cavity_name+"/cavity_linear/0/*.vtu"
+    vtu_files=glob.glob(search_phrase)
+    if (len(vtu_files) == 0):
+      print("no vtu was found in ", search_phrase, ". quiting!")
+      sys.exit("Exiting!")
+    cavity_path=vtu_files[0]
+    cavity_wire_path=cavity_path
+
+    search_phrase=root+"/cavities/"+cavity_name+"/entity_linear/0/*.vtu"
+    vtu_files=glob.glob(search_phrase)
+    if (len(vtu_files) == 0):
+      print("no vtu was found in ", search_phrase, ". quiting!")
+      sys.exit("Exiting!")
+    entity_path=vtu_files[0]
+  return cavity_path, cavity_wire_path, entity_path
+# end of get_vtu_path_for_cavity
+
+
 # construct the mesh_path
-# for folder name of the form "rep_tri_nnn_levels"
-# where nnn is the resolutions pick the one with max "nnn"
-search_phrase=args.root_dir+"/mesh/curved/rep_tri_*"
-all_rep_tri_folders=glob.glob(search_phrase)
-target_dir = ''
-if (len(all_rep_tri_folders) == 1):
-  target_dir=all_rep_tri_folders[0]
-else:
-  max_res=-1
-  max_idx=0
-  idx=0
-  for st in all_rep_tri_folders:
-    substs = st.split("_")
-    if substs[-2] > max_res:
-      max_res = substs[-2]
-      max_idx = idx
-    idx=idx+1
-  target_dir=all_rep_tri_folders[max_idx]
+# vtu_phrase="vtu/order_*"
+mesh_path=get_vtu_path_for_mesh(args.root_dir, "vtu/order_*")
 
-# for file name of the form order_nnn_0.vtu
-# where nnn is the order of the mesh, find the one with max "nnn"
-search_phrase=target_dir+"/vtu/order_*.vtu"
-all_vtus=glob.glob(search_phrase)
-
-if (len(all_vtus) == 0):
-  print("no vtu was found in ", search_phrase, ". quiting!")
-  sys.exit("Exiting!")
-
-target_vtu=''
-if (len(all_vtus) == 1):
-  target_vtu=all_vtus[0]
-else:
-  max_res=-1
-  max_idx=0
-  idx=0
-  for st in all_vtus:
-    substs = st.split("_")
-    if substs[-2] > max_res:
-      max_res = substs[-2]
-      max_idx = idx
-    idx=idx+1
-  target_vtu=all_vtus[max_idx]
-
-mesh_path=target_vtu
-
-# construct the paths related to cavity
-# these include cavity_path cavity_wire_path entity_path
-search_phrase=args.root_dir+"/cavities/"+args.cavity_name+"/cavity_curved/rep_tri_*"
-all_rep_tri_folders=glob.glob(search_phrase)
-
-if (len(all_rep_tri_folders) == 0):
-  print("no cavity fount for  ", args.cavity_name)
-  names_path = glob.glob(args.root_dir+"/cavities/*")
-  names = [name.split("/")[-1] for name in names_path]
-  print("available names are:")
-  print(names)
-  sys.exit("Exiting!")
-
-
-target_dir = ''
-if (len(all_rep_tri_folders) == 1):
-  target_dir=all_rep_tri_folders[0]
-else:
-  max_res=-1
-  max_idx=0
-  idx=0
-  for st in all_rep_tri_folders:
-    substs = st.split("_")
-    if substs[-2] > max_res:
-      max_res = substs[-2]
-      max_idx = idx
-    idx=idx+1
-  target_dir=all_rep_tri_folders[max_idx]
-
-search_phrase=target_dir+"/vtu/order_*.vtu"
-all_vtus=glob.glob(search_phrase)
-
-if (len(all_vtus) == 0):
-  print("no vtu was found in ", search_phrase, ". quiting!")
-  sys.exit("Exiting!")
-
-
-target_vtu=''
-if (len(all_vtus) == 1):
-  target_vtu=all_vtus[0]
-else:
-  max_res=-1
-  max_idx=0
-  idx=0
-  for st in all_vtus:
-    substs = st.split("_")
-    if substs[-2] > max_res:
-      max_res = substs[-2]
-      max_idx = idx
-    idx=idx+1
-  target_vtu=all_vtus[max_idx]
-
-cavity_path=target_vtu
-
-search_phrase=args.root_dir+"/cavities/"+args.cavity_name+"/cavity_curved_wire/cavity_curved_wire.pvtu"
-pvtu_files=glob.glob(search_phrase)
-if (len(pvtu_files) == 0):
-  print("no pvtu was found in ", search_phrase, ". quiting!")
-  sys.exit("Exiting!")
-
-cavity_wire_path=pvtu_files[0]
-
-search_phrase=args.root_dir+"/cavities/"+args.cavity_name+"/entity_curved_wire/entity_curved_wire.pvtu"
-entity_pvtu=glob.glob(search_phrase)
-
-if (len(entity_pvtu) == 0):
-  print("no pvtu was found for cavity  ", args.cavity_name, ". quiting!")
-  sys.exit("Exiting!")
-entity_path=entity_pvtu[0]
-print(entity_path)
-
+cavity_path, cavity_wire_path, entity_path = get_vtu_path_for_cavity(args.root_dir, args.cavity_name)
 
 #### ParaView Related Code
 
@@ -176,7 +201,7 @@ order_3_0vtu_1Display.ScalarOpacityUnitDistance = 0.05
 order_3_0vtu_1Display.Opacity = 0.5
 
 # create a new 'XML Partitioned Unstructured Grid Reader'
-cavity_curved_wirepvtu = XMLPartitionedUnstructuredGridReader(FileName=[cavity_wire_path])
+cavity_curved_wirepvtu = XMLUnstructuredGridReader(FileName=[cavity_wire_path])
 cavity_curved_wirepvtu.CellArrayStatus = ['apf_part']
 
 # show data in view
@@ -194,7 +219,7 @@ cavity_curved_wirepvtuDisplay.DiffuseColor = [0.0, 0.0, 0.0]
 # Properties modified on cavity_curved_wirepvtuDisplay
 cavity_curved_wirepvtuDisplay.LineWidth = 2.0
 
-entity_curved_wirepvtu = XMLPartitionedUnstructuredGridReader(FileName=[entity_path])
+entity_curved_wirepvtu = XMLUnstructuredGridReader(FileName=[entity_path])
 entity_curved_wirepvtu.CellArrayStatus = ['apf_part']
 
 # show data in view
